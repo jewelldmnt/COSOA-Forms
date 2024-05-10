f7b706b08cf179e751b36093069218d1bc3e8ff2
mkas020103 <mdfl020103@gmail.com>
2024-05-11 00:28:40 +0800

    created a layout.html
    place here all the same blocks of code in each forms

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('gpoa.html')
+
+@app.route("/sign")
+def sign():
+    return render_template('gpoa.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
